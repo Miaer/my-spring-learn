4ca8080c0cb27fcd0d2153dcef04f747ed12e455
mrliz <mr.lizegan@outlook.com>
2020-05-23 18:48:30 +0800

    init

---
+++ aop/src/main/java/springboot/learning/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.learning.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
